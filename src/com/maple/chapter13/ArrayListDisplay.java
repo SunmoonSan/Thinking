41161f4e75d74179be62570da0982d54f6e0dac3
San <18814184179@163.com>
2018-03-10 23:33:42 +0800

    字符串

---
+++ src/com/maple/chapter13/ArrayListDisplay.java
@@ -0,0 +1,9 @@
+package com.maple.chapter13;
+
+import java.util.ArrayList;
+
+public class ArrayListDisplay {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/maple/chapter13/UsingStringBuilder.java
@@ -0,0 +1,25 @@
+package com.maple.chapter13;
+
+import java.util.Random;
+
+public class UsingStringBuilder {
+    public static Random random = new Random(47);
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder("[");
+        for (int i=0; i<25; i++) {
+            result.append(random.nextInt(100));
+            result.append(", ");
+        }
+
+        result.delete(result.length() -2, result.length());
+        result.append("]");
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        UsingStringBuilder usb = new UsingStringBuilder();
+        System.out.println(usb);
+    }
+}
